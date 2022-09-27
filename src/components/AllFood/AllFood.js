c8d418c590fe75841b581bd024cd2597e72da517
RayhanalKavey <rayhanalkavey@gmail.com>
2022-09-28 04:43:05 +0600

    Add food cards with informations and style it.

--- src/components/AllFood/AllFood.js
+++ src/components/AllFood/AllFood.js
@@ -3,19 +3,25 @@ import Food from "../Food/Food";
 import "./AllFood.css";
 const AllFood = () => {
   const [foods, setFoods] = useState([]);
-  const url = "https://www.themealdb.com/api/json/v1/1/search.php?s=";
   useEffect(() => {
+    const url = `https://www.themealdb.com/api/json/v1/1/search.php?s=`;
     fetch(url)
       .then((res) => res.json())
       .then((data) => setFoods(data.meals));
   }, []);
+  // const search=()=>{
 
+  // }
   return (
     <div>
-      <h1>This is all food</h1>
-      {foods.map((food) => (
-        <Food food={food} key={food.idMeal}></Food>
-      ))}
+      <h1 className="text-5xl text-center my-5">
+        Foods from different country
+      </h1>
+      <div className="grid md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-5 justify-center justify-items-center		">
+        {foods.map((food) => (
+          <Food food={food} key={food.idMeal}></Food>
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/Food/Food.js
+++ src/components/Food/Food.js
@@ -1,8 +1,38 @@
 import React from "react";
 import "./Food.css";
 const Food = ({ food }) => {
-  console.log(food);
-  return <div></div>;
+  // console.log(food);
+  const {
+    strMeal,
+    strInstructions,
+    strTags,
+    strArea,
+    idMeal,
+    strMealThumb,
+    strSource,
+    strYoutube,
+  } = food;
+  return (
+    <div className="card w-72 bg-base-100 shadow-xl ">
+      <figure>
+        <img className="w-80 h-52" src={strMealThumb} alt="Shoes" />
+      </figure>
+      <div className="card-body">
+        <h2 className="card-title uppercase font-bold text-2xl">{strMeal}</h2>
+        <p>
+          <span className="uppercase font-bold">Procedure:</span>{" "}
+          {strInstructions.slice(0, 100)}
+        </p>
+        <p>
+          <span className="uppercase font-bold">Food Area:</span> {strArea}
+        </p>
+        <div className="flex justify-between">
+          <button className="btn btn-primary">Add to cart</button>
+          <button className="btn btn-primary">Details</button>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Food;
